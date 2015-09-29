aa000bb075720164bc10dce3ab366bbd51467e26
Samuel-Lewis <mazuel@gmail.com>
2015-09-29 22:31:54 +0930

    + Added lbRNG, a random number generator * Moved all the examples to one file * Updated the Makefile to do both the example, and act as a better Makefile that can plug & play * Updated lbLog to log string streams now (super hacky solution though) - removed old examples - removed 'generic makefile' nonsense

---
+++ include/lbRNG.h
@@ -0,0 +1,86 @@
+//===============================================//
+//
+//	@Author:	Samuel Lewis
+//	@Project:	https://github.com/Samuel-Lewis/lunchbox
+//			
+//	@Last Updated: 2015-09-29 20:04:07
+//	@Created:      2015-07-14 21:26:08
+//
+//===============================================//
+
+#ifndef _LB_RNG_H
+#define _LB_RNG_H
+
+#include <random>
+#include <cmath>
+#include <iostream>
+
+#define TIME_OUT 100
+
+class lbRNG
+{
+public:
+	template<typename T>
+	static T linear(T,T);
+
+	template <typename T>
+	static T normDist(T,T);
+
+	template <typename T>
+	static T normDist(T,T,T,T);
+
+};
+
+// LINEAR
+template <typename T>
+T lbRNG::linear(T lower, T upper)
+{
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::uniform_real_distribution<> distribution(lower, upper);
+
+	T randNum = distribution(generator);
+
+	return randNum;
+
+}
+
+// NORMAL DISTRIBUTION
+template <typename T>
+T lbRNG::normDist(T mean, T stdDev)
+{
+	// Using C++ random engine
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::normal_distribution<> distribution(mean, stdDev);
+
+	T randNum = distribution(generator);
+
+	return randNum;
+}
+
+template <typename T>
+T lbRNG::normDist(T mean, T stdDev, T lower, T upper)
+{
+	// Using C++ random engine
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::normal_distribution<> distribution(mean, stdDev);
+
+	T randNum = distribution(generator);
+	for (int i = 0; i < TIME_OUT; i++)
+	{
+		// Not in while loop, because if user doesn't make the right range, we can't just inifite loop.
+		if (randNum <= lower || randNum >= upper)
+		{
+			// Make sure we are in the designated range. Else get the number again.
+			randNum = distribution(generator);
+		} else {
+			break;
+		}
+	}
+
+	return randNum;
+}
+
+#endif //_LB_RNG_H
