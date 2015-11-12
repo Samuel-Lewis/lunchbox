c744189a114d3b5abb22f16a1c49f3efe3724ed9
Samuel-Lewis <mazuel@gmail.com>
2015-11-12 16:36:44 +1030

    *Updated to match lbLog’s new LOG level * Formatting in example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -5,7 +5,7 @@
 //
 //	@Project:	Lunchbox Toolset
 //
-//	@Last Updated:	2015-10-19 17:41:52
+//	@Last Updated:	2015-11-12 16:34:28
 //	@Created:		2015-09-30 13:45:13
 //
 //===============================================//
@@ -22,18 +22,18 @@
 
 int main()
 {
-	// ==== lbLog demo ==== 
+	// ==== lbLog demo ==== //
 	// Set debug levels to show all, log all, and quit on fatal
 	lbLog::quickSet(lbLog::info, lbLog::all, lbLog::fatal);
 
 	// Start the log in root directory, named 'testlog-[timestamp].log'
 	lbLog::startLog("example/logs/","testlog",true);
 
-	// ==== lbRNG demo ==== 
+	// ==== lbRNG demo ==== //
 	INFO("A random number between 0 and 10 is " << lbRNG::linear(0,10));
-	INFO("A normally distrubuted number mean = 5, std dev = 2:  " << lbRNG::normDist(5,2));
+	INFO("A normally distributed N(5,2)" << lbRNG::normDist(5,2));
 
-	// ==== lbString demo ==== 
+	// ==== lbString demo ==== //
 	std::string str = "RACE car";
 	std::vector<std::string> words = lbString::split(str, " ");
 
@@ -45,7 +45,7 @@ int main()
 		WARN(words[i]);
 	}
 
-	// ==== lbCSV demo ==== 
+	// ==== lbCSV demo ==== //
 	// fakelist.csv doens't exist, will thrown an error
 	TABLE fakeList = lbCSV::read("fakelist.csv");
 	TABLE list = lbCSV::read("example/list.csv");
